f81e54c6c766aea6c89150f64e9cfe9be0a94e03
Ilyas Founas <ton_email@example.com>
2026-04-26 15:23:31 +0200

    try to repair my git repo

--- srcs/requirements/nginx/certif_ssl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mkdir -p /etc/nginx/ssl
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-  -keyout /etc/nginx/ssl/ifounas.key \
-  -out /etc/nginx/ssl/ifounas.crt \
-  -subj "/CN=ifounas.42.fr"
